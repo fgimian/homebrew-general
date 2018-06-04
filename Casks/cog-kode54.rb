6fa7b770a1c9b57f4d69efac181456bc54a9b5b7
Fotis Gimian <fgimiansoftware@gmail.com>
2018-06-04 21:02:54 +1000

    Updated Cog to the latest version k54-972-g53fa8e2a

--- Casks/cog-kode54.rb
+++ Casks/cog-kode54.rb
@@ -1,6 +1,6 @@
 cask 'cog-kode54' do
-  version 'k54-969,gfdf3ae9b'
-  sha256 '7f4644119aae5b0354e6e907126820ac54686a639bfa2cce475bbb9e245ce6fb'
+  version 'k54-972,g53fa8e2a'
+  sha256 'd5c41e08ceecf40a1d3dc46f67b7e15f8f236ca39b849102630c29d533ead76e'
 
   # f.losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma[1..-1]}.zip"
