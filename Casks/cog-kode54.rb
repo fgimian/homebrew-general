2cecfc021bdfd4e7e82e8ad606c47ac2cc907323
Fotis Gimian <fgimiansoftware@gmail.com>
2018-06-08 19:10:45 +1000

    Updated Cog app to version k54-973-ga429025d

--- Casks/cog-kode54.rb
+++ Casks/cog-kode54.rb
@@ -1,6 +1,6 @@
 cask 'cog-kode54' do
-  version 'k54-972,g53fa8e2a'
-  sha256 'd5c41e08ceecf40a1d3dc46f67b7e15f8f236ca39b849102630c29d533ead76e'
+  version 'k54-973,ga429025d'
+  sha256 'aa07f341a113d9acb31bb7731ed14785b516e79d3ca026925b1e56a7d94b538e'
 
   # f.losno.co/cog was verified as official when first introduced to the cask
   url "https://f.losno.co/cog/Cog-#{version.after_comma[1..-1]}.zip"
